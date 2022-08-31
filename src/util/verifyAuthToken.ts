ebba9c36d9fe782dfe7f59d993349756e266d0c4
Akshay Gupta <gravity.akshay@gmail.com>
2022-08-31 21:09:21 +0530

    Add verifyAuthToken.ts

---
+++ src/util/verifyAuthToken.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+
+const verifyAuthToken = (_req: Request, res: Response, next: any): void => {
+  try {
+    const authorizationHeader = _req.headers.authorization;
+    const token: string = (authorizationHeader as string).split(' ')[1];
+    jwt.verify(token, process.env.TOKEN_SECRET as jwt.Secret);
+    next();
+  } catch (error) {
+    res.status(401);
+  }
+};
+
+export default verifyAuthToken;
