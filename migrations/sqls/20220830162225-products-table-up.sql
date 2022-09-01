8822a897474057875e4bd14c314a20f3f6ce5cca
Akshay Gupta <gravity.akshay@gmail.com>
2022-09-02 01:26:52 +0530

    Remove colon from products handler

--- migrations/sqls/20220830162225-products-table-up.sql
+++ migrations/sqls/20220830162225-products-table-up.sql
@@ -2,6 +2,6 @@ CREATE TABLE products (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     price integer,
-    category: VARCHAR(200),
+    category VARCHAR(200),
     orderscount integer
 );
